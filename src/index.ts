1683986c2b15fe041504c10d6ee80178d9f23147
Junior Ventura <juniorventura77@gmail.com>
2023-02-08 10:38:08 -0400

    feat: adding 2 get enpoints just for the sake of explanation

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,43 @@ app.get('/ping', (req, res) => {
     res.send('pong updated');
 });
 
+// Return all cars, with a in-memery collection
+
+const carsArray = [
+    {
+        id: 1, brand: "toyota"
+    },
+    {
+        id: 2, brand: "honda"
+    },
+    {
+        id: 3, brand: "nissan"
+    },
+    {
+        id: 4, brand: "mazda"
+    }
+]
+
+// Return all cars
+
+app.get('/api/cars', (_, res) => {
+    res.send(carsArray);
+});
+
+// Return a given car by id
+
+app.get('/api/cars/:id', (req, res) => {
+
+    const id = req.params.id;
+    const { id: carId } = req.params;
+
+    const carFound = carsArray.filter(x => x.id === +id);
+
+    res.send(carFound);
+});
+
+
+
 // Set the port for the express server, also a callback, a function that is executed when the server initilizes.
 app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`);
